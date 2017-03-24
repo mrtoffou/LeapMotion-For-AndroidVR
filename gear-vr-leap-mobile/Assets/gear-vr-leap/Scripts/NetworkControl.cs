1549b666b94bc150fdd6169602dc6bcb2d58a78f
John Lam <jlam1@ggc.edu>
2017-03-24 09:43:16 -0400

    init repo

---
+++ gear-vr-leap-mobile/Assets/gear-vr-leap/Scripts/NetworkControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class NetworkControl : MonoBehaviour
+{
+	public GameObject startServer;
+	public GameObject createRoom;
+	public GameObject ipText;
+	NetworkManager manager;
+	// Use this for initialization
+	void Start()
+	{
+		manager = GetComponent<NetworkManager>();
+		createRoom.SetActive(false);
+		ipText.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+	}
+
+	public void StartServer()
+	{
+		startServer.SetActive(false);
+		createRoom.SetActive(true);
+		ipText.SetActive(true);
+	}
+
+
+	public void CreateRoom()
+	{
+		if (!NetworkClient.active && !NetworkServer.active && manager.matchMaker == null)
+		{
+			manager.StartServer();
+		}
+	}
+
+}
